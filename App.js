2ba573aa0f9bca2324198f74b9d721ee25197263
Jonikko <nikxuz@gmail.com>
2020-10-11 16:01:24 +0800

    nikkoSample

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
   return (
     <View>
-      <Text>Test</Text>
+      <Text>bogo ko</Text>
     </View>
   )
 }
